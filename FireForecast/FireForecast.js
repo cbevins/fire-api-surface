00c3b7b678b3689fefc7968b682c16ad9f3e066a
Collin D. Bevins <cbevins@montana.com>
2021-05-06 13:36:43 -0600

    Refactored

--- FireForecast/FireForecast.js
+++ FireForecast/FireForecast.js
@@ -9,9 +9,8 @@
  * - weatherapi.com
  */
 import moment from 'moment'
-import { FuelMoisture as Fm } from '@cbevins/fire-behavior-simulator'
 import { mapquestElevSlopeAspect as mapQuest } from '../Globe/elevQuery-mapquest.js'
-import { fireBehavior } from './fireBehavior.js'
+import { FireBehavior } from './fireBehavior.js'
 import { getTimelines as getTomorrow } from './wxQuery-tomorrow.js'
 import { getForecast as getWeatherapi } from './wxQuery-weatherapi.js'
 
@@ -20,47 +19,34 @@ import { getForecast as getWeatherapi } from './wxQuery-weatherapi.js'
 // - use elevation service
 
 // Adds fire behavior to the weather records
-function addFireBehavior (wxArray, parms) {
-  addFuelMoisture(wxArray, parms)
+function addFireBehavior (parms, wxArray) {
   wxArray.forEach(wx => {
     const input = {
       fuel: parms.fuel,
       curedHerb: 0.01 * parms.cured,
-      tl1h: 0.01 * wx.fosbergDead1h,
-      tl10h: 0.01 * wx.fosbergDead10h,
-      tl100h: 0.01 * wx.fosbergDead100h,
-      liveMoisture: 0.01 * parms.live,
-      windAt10m: wx.windSpeed,
-      windAdj: parms.waf,
+      month: +(wx.date).substr(5, 2),
+      hour: +(wx.time).substr(0, 2),
+      elevDiff: parms.elevdiff,
       aspect: parms.aspect,
       slope: 0.01 * parms.slope,
-      windFrom: wx.windFrom,
       dryBulb: wx.dryBulb,
+      humidity: 0.01 * wx.humidity,
+      shading: 0.01 * wx.cloudCover,
+      liveMoisture: 0.01 * parms.live,
+      windAt10m: 88 * wx.windSpeed,
+      windGust: 88 * wx.windGust,
+      windAdj: parms.waf,
+      windFrom: wx.windFrom,
       elapsed: 60
     }
-    const output = fireBehavior(input)
-    wx.spreadRate = output.heading.spreadRate
-    wx.flameLength = output.heading.flameLength
-    wx.scorchHeight = output.heading.scorchHeight
-  })
-  return wxArray
-}
-
-// Adds the 5 Fosberg fuel moisture values to each weather record
-function addFuelMoisture (wxArray, parms) {
-  wxArray.forEach(w => {
-    const month = +(w.date).substr(5, 2)
-    const hour = +(w.time).substr(0, 2)
-    const shading = 0.01 * w.cloudCover
-    const aspect = parms.aspect
-    const slope = parms.slope // %
-    const elevDiff = 0
-    // These require humidity, slope, shading as ratios and returns moisture as ratio
-    w.fosbergDead1hRef = 100 * Fm.fosbergReference(w.dryBulb, 0.01 * w.humidity)
-    w.fosbergDead1hAdj = 100 * Fm.fosbergCorrection(month, shading, aspect, slope, hour, elevDiff)
-    w.fosbergDead1h = 100 * Fm.fosbergDead1h(0.01 * w.fosbergDead1hRef, 0.01 * w.fosbergDead1hAdj)
-    w.fosbergDead10h = 100 * Fm.fosbergDead10h(0.01 * w.fosbergDead1h)
-    w.fosbergDead100h = 100 * Fm.fosbergDead100h(0.01 * w.fosbergDead1h)
+    const output = parms.fireBehavior.run(input)
+    wx.tl1h = 100 * output.moisture.fosberg.tl1h // ratio
+    wx.tl10h = 100 * output.moisture.tl10h // ratio
+    wx.tl100h = 100 * output.moisture.tl100h // ratio
+    wx.spreadRate = output.heading.spreadRate // ft/min
+    wx.flameLength = output.heading.flameLength // ft
+    wx.scorchHeight = output.heading.scorchHeight // ft
+    wx.headingFromNorth = output.fire.headingFromNorth // degrees
   })
   return wxArray
 }
@@ -69,8 +55,8 @@ function fix (v, w, d, c = ' ') {
   return (typeof v === 'string') ? v.padStart(w, c) : v.toFixed(d).padStart(w, c)
 }
 
-// Returns the forecast header
-function forecastHeader (parms) {
+// Returns the forecast table string
+function getForecastTable (parms, wxArray) {
   let str = `\nFire Forecast for '${parms.name}' at lat ${parms.lat}, lon ${parms.lon}:\n`
   str += `  Elev:       ${fix(parms.elev, 4, 0)} ft\n`
   str += `  Slope:      ${fix(parms.slope, 4, 0)} %\n`
@@ -79,47 +65,31 @@ function forecastHeader (parms) {
   str += `  Cured Herb: ${fix(parms.cured, 4, 2)} %\n`
   str += `  Live Moist: ${fix(parms.live, 4, 0)} %\n`
   str += `  Wind Adj:   ${fix(parms.waf, 4, 2)}\n`
-  str += '| Date       Time  |  Db  Rh | Wind f/s  | Cld | Dead Fuel Moisture | Spread  Flame Scorch |\n'
-  str += '| Year-Mo-Dy Hr:Mn |  oF   % | Sp Gs Dir | Cvr | 1h ( R+C) 10h 100h | ft/min     ft     ft |\n'
-  str += '|------------------|---------|-----------|-----|--------------------|----------------------|'
-  return str
-}
-
-// Formats and returns a single hourly weather & fire forecast row
-function forecastRow (w) {
-  let str = `| ${w.date} ${w.time} | `
-  str += `${fix(w.dryBulb, 3, 0)} `
-  str += `${fix(w.humidity, 3, 0)} | `
-  str += `${fix(w.windSpeed, 2, 0)} `
-  str += `${fix(w.windGust, 2, 0)} `
-  str += `${fix(w.windFrom, 3, 0)} | `
-  str += `${fix(w.cloudCover, 3, 0)} | `
-  // str += `${fix(w.solarGHI, 5, 0)} | `
-  str += `${fix(w.fosbergDead1h, 2, 0)} (`
-  str += `${fix(w.fosbergDead1hRef, 2, 0)}+`
-  str += `${fix(w.fosbergDead1hAdj, 1, 0)}) `
-  str += `${fix(w.fosbergDead10h, 3, 0)} `
-  str += `${fix(w.fosbergDead100h, 3, 0)}  | `
-  str += `${fix(w.spreadRate, 6, 2)} `
-  str += `${fix(w.flameLength, 6, 2)} `
-  str += `${fix(w.scorchHeight, 6, 2)} |`
-  return str
-}
-
-// Adds fuel moisture and fire behavior to forecast, then displays it
-function showForecast (wxArray, parms) {
-  addFireBehavior(wxArray, parms)
-  // console.log(wxArray)
-  let str = forecastHeader(parms) + '\n'
-  wxArray.forEach(wx => {
-    str += forecastRow(wx) + '\n'
+  str += '| Date       Time  |  Db  Rh | Wind mph  | Cld |  Dead Fuel  | Spread  Flame Scorch Head |\n'
+  str += '| Year-Mo-Dy Hr:Mn |  oF   % | Sp Gs Dir | Cvr | 1h 10h 100h | ft/min     ft     ft  No  |\n'
+  str += '|------------------|---------|-----------|-----|-------------|---------------------------|\n'
+  wxArray.forEach(w => {
+    str += `| ${w.date} ${w.time} | `
+    str += `${fix(w.dryBulb, 3, 0)} `
+    str += `${fix(w.humidity, 3, 0)} | `
+    str += `${fix(w.windSpeed, 2, 0)} `
+    str += `${fix(w.windGust, 2, 0)} `
+    str += `${fix(w.windFrom, 3, 0)} | `
+    str += `${fix(w.cloudCover, 3, 0)} | `
+    // str += `${fix(w.solarGHI, 5, 0)} | `
+    str += `${fix(w.tl1h, 2, 0)} `
+    str += `${fix(w.tl10h, 3, 0)} `
+    str += `${fix(w.tl100h, 3, 0)}  | `
+    str += `${fix(w.spreadRate, 6, 2)} `
+    str += `${fix(w.flameLength, 6, 2)} `
+    str += `${fix(w.scorchHeight, 6, 2)} `
+    str += `${fix(w.headingFromNorth, 3, 0)} |\n`
   })
-  console.log(str)
   return str
 }
 
 async function fireForecast (parms) {
-  // First get elevation, slope aspect
+  // First get elevation, slope, and aspect and add it to the parms
   const sampleRes = 1 / (60 * 60 * 3) // 1/3 arc-second in decimal degrees
   const cellWidth = 2 // Double sample distance to ensure adjacent cells have different sample
   const esa = await mapQuest(parms.lat, parms.lon, sampleRes, cellWidth)
@@ -127,10 +97,13 @@ async function fireForecast (parms) {
   parms.slope = 100 * esa.slopeRatio
   parms.aspect = esa.aspect
 
+  // Next get weather data from tomorrow.io or weatherapi.com
   // getTomorrow(parms.lat, parms.lon, parms.start, parms.end, parms.timezone)
   //   .then(result => { showForecast(result, parms) })
-  const wx = await getWeatherapi(parms.lat, parms.lon, 1, 'fire')
-  showForecast(wx, parms)
+  const wxArray = await getWeatherapi(parms.lat, parms.lon, 1, 'fire')
+  addFireBehavior(parms, wxArray)
+  const table = getForecastTable(parms, wxArray)
+  console.log(table)
 }
 
 // ----------------------------------------------------------------
@@ -144,7 +117,7 @@ const loc = M
 const now = moment.utc()
 const parms = {
   name: loc.name,
-  // weather service parameters
+  // weather and elevation service input parameters
   lat: loc.lat,
   lon: loc.lon,
   start: moment.utc(now).startOf('hour').toISOString(), // "2019-03-20T14:09:50Z"
@@ -152,14 +125,13 @@ const parms = {
   // Timezone of time values, according to IANA Timezone Names (defaults to 'UTC')
   // https://docs.tomorrow.io/reference/api-formats#timezone
   timezone: 'America/Denver',
-  // elevation service parameters
-  slope: 20, // slope steepness (%)
-  aspect: 180, // aspect
-  // fire behavior parameters
+
+  // fire behavior input parameters
   fuel: 'gs1',
   waf: 0.4,
   cured: 0, // herb cured fraction (%)
-  live: 200 // live herb and stem fuel moisture (%)
+  live: 200, // live herb and stem fuel moisture (%)
+  elevdiff: 0
 }
 
 // Process any actual command line parameters
@@ -173,5 +145,6 @@ for (let a = 2; a < process.argv.length; a++) {
   }
 }
 // \TODO - validate command line args
+parms.fireBehavior = new FireBehavior()
 fireForecast(parms)
 console.log('Getting elevation and weather data ...')
--- FireForecast/fireBehavior.js
+++ FireForecast/fireBehavior.js
@@ -4,134 +4,149 @@
  * @author Collin D. Bevins, <cbevins@montana.com>
  * @license MIT
 */
-// Step 1: import the fire-behavior-simulator package from node_modules or source folder
 import { Sim } from '@cbevins/fire-behavior-simulator'
 
-export function fireBehavior (inp) {
-  // Step 2 - create a fire behavior simulator with 1 directed acyclical graph (DAG)
-  const sim = new Sim()
-  const dag = sim.createDag('FireForecast')
+export class FireBehavior {
+  constructor () {
+    this.sim = new Sim()
+    this.dag = this.sim.createDag('FireForecast')
+    this.dag.configure([
+      ['configure.fuel.primary', 'catalog'], // The primary fuel is specified by a fuel model catalog key
+      ['configure.fuel.secondary', 'none'], // There are no secondary fuels
+      ['configure.fuel.moisture', 'fosberg'], // 3 dead moisture classes and a singe live category moisture
+      ['configure.fuel.curedHerbFraction', 'input'],
+      ['configure.wind.speed', 'at10m'],
+      ['configure.wind.direction', 'sourceFromNorth'],
+      ['configure.slope.steepness', 'ratio'],
+      ['configure.fuel.windSpeedAdjustmentFactor', 'input'],
+      ['configure.fire.vector', 'fromHead'],
+      ['configure.temperature.humidity', 'humidity'], // enter dry bulb and humidity
+      ['configure.fuel.chaparralTotalLoad', 'input'], // unimportant
+      ['configure.fire.weightingMethod', 'arithmetic'], // unimportant
+      ['configure.fire.effectiveWindSpeedLimit', 'ignored'],
+      ['configure.fire.firelineIntensity', 'flameLength'],
+      ['configure.fire.lengthToWidthRatio', 'lengthToWidthRatio']
+    ])
 
-  // Step 3 - configure input choices and computational options
-  dag.configure([
-    ['configure.fuel.primary', 'catalog'], // The primary fuel is specified by a fuel model catalog key
-    ['configure.fuel.secondary', 'none'], // There are no secondary fuels
-    ['configure.fuel.moisture', 'liveCategory'], // 3 dead moisture classes and a singe live category moisture
-    ['configure.fuel.curedHerbFraction', 'input'],
-    ['configure.wind.speed', 'at10m'],
-    ['configure.wind.direction', 'sourceFromNorth'],
-    ['configure.slope.steepness', 'ratio'],
-    ['configure.fuel.windSpeedAdjustmentFactor', 'input'],
-    ['configure.fire.vector', 'fromHead'],
-    ['configure.temperature.humidity', 'humidity'], // enter dry bulb and humidity
-    ['configure.fuel.chaparralTotalLoad', 'input'], // unimportant
-    ['configure.fire.weightingMethod', 'arithmetic'], // unimportant
-    ['configure.fire.effectiveWindSpeedLimit', 'ignored'],
-    ['configure.fire.firelineIntensity', 'flameLength'],
-    ['configure.fire.lengthToWidthRatio', 'lengthToWidthRatio']
-  ])
-
-  // Step 4 - select the fire behavior variables (DagNodes) of interest
-  // (See ./docs/Variables.md for complete list of 1200+ names)
-  const selected = [
-    'surface.primary.fuel.model.behave.parms.cured.herb.fraction', // ratio
-    'surface.primary.fuel.fire.effectiveWindSpeed', // ft/min
-    'surface.primary.fuel.fire.flameResidenceTime', // min
-    'surface.primary.fuel.fire.heading.fromUpslope', // degrees
-    'surface.primary.fuel.fire.heading.fromNorth', // degrees
-    'surface.primary.fuel.fire.heatPerUnitArea', // btu/ft2 |
-    'surface.primary.fuel.fire.reactionIntensity', // btu/ft2/min
-    'surface.fire.ellipse.axis.lengthToWidthRatio', // ratio
-    'surface.fire.ellipse.back.firelineIntensity', // Btu/ft/s
-    'surface.fire.ellipse.back.flameLength', // ft
-    'surface.fire.ellipse.back.scorchHeight', // ft
-    'surface.fire.ellipse.back.spreadDistance', // ft
-    'surface.fire.ellipse.back.spreadRate', // ft/min
-    'surface.fire.ellipse.flank.firelineIntensity',
-    'surface.fire.ellipse.flank.flameLength',
-    'surface.fire.ellipse.flank.scorchHeight',
-    'surface.fire.ellipse.flank.spreadDistance',
-    'surface.fire.ellipse.flank.spreadRate',
-    'surface.fire.ellipse.head.firelineIntensity',
-    'surface.fire.ellipse.head.flameLength',
-    'surface.fire.ellipse.head.scorchHeight',
-    'surface.fire.ellipse.head.spreadDistance',
-    'surface.fire.ellipse.head.spreadRate',
-    'surface.fire.ellipse.size.area', // ft2
-    'surface.fire.ellipse.size.length', // ft
-    'surface.fire.ellipse.size.perimeter', // ft
-    'surface.fire.ellipse.size.width' // ft
-  ]
-  dag.select(selected)
-
-  // Step 5 - determine the required input variables
-
-  // You can request an array of the configurations currently applicable to your selected variables:
-  // const activeConfigs = dag.requiredConfigNodes() // returns an array of DagNode references
-  // console.log('ACTIVE CONFIGS:')
-  // activeConfigs.forEach(cfg => { console.log(cfg.key(), cfg.value()) })
-
-  // You will initially need to see exactly which inputs are required
-  // for your currently selected variables and configuration settings
-  // const requiredInputs = dag.requiredInputNodes() // returns an array of DagNode references
-  // console.log('REQUIRED INPUTS:')
-  // requiredInputs.forEach(node => { console.log(node.key()) })
-
-  // Step 6 - specify the values of the required inputs and run()
-  dag.input([
-    ['surface.primary.fuel.model.catalogKey', [inp.fuel]],
-    ['surface.primary.fuel.model.behave.parms.cured.herb.fraction', [inp.curedHerb]], // fraction
-    ['site.moisture.dead.tl1h', [inp.tl1h]], // fraction of fuel ovendry weight
-    ['site.moisture.dead.tl10h', [inp.tl10h]], // fraction of fuel ovendry weight
-    ['site.moisture.dead.tl100h', [inp.tl100h]], // fraction of fuel ovendry weight
-    ['site.moisture.live.category', [inp.liveMoisture]], // fraction of fuel ovendry weight
-    ['site.wind.speed.at10m', [88 * inp.windAt10m]], // feet per minute (1 mph = 88 ft/min)
-    ['site.windSpeedAdjustmentFactor', [inp.windAdj]], // fraction of 10m wind speed
-    ['site.slope.direction.aspect', [inp.aspect]], // degrees clockwise from north
-    ['site.slope.steepness.ratio', [inp.slope]], // vertical rise / horizontal reach
-    ['site.wind.direction.source.fromNorth', [inp.windFrom]], // direction of wind origin, degrees clockwise from north
-    ['site.temperature.air', [inp.dryBulb]], // oF
-    ['site.fire.time.sinceIgnition', [inp.elapsed]] // minutes
-  ]).run()
-
-  // Step 7 - return the single result set
-  const output = {
-    fire: {
-      effectiveWindSpeed: dag.node('surface.primary.fuel.fire.effectiveWindSpeed').value(), // ft/min
-      flameResidenceTime: dag.node('surface.primary.fuel.fire.flameResidenceTime').value(), // min
-      headingFromUpslope: dag.node('surface.primary.fuel.fire.heading.fromUpslope').value(), // degrees
-      headingFromNorth: dag.node('surface.primary.fuel.fire.heading.fromNorth').value(), // degrees
-      heatPerUnitArea: dag.node('surface.primary.fuel.fire.heatPerUnitArea').value(), // btu/ft2 |
-      reactionIntensity: dag.node('surface.primary.fuel.fire.reactionIntensity').value() // btu/ft2/min
-    },
-    ellipse: {
-      lwRatio: dag.node('surface.fire.ellipse.axis.lengthToWidthRatio').value(), // ratio
-      area: dag.node('surface.fire.ellipse.size.area').value(), // ft2
-      length: dag.node('surface.fire.ellipse.size.length').value(), // ft
-      perimeter: dag.node('surface.fire.ellipse.size.perimeter').value(), // ft
-      width: dag.node('surface.fire.ellipse.size.width').value() // ft
-    },
-    backing: {
-      firelineIntensity: dag.node('surface.fire.ellipse.back.firelineIntensity').value(), // Btu/ft/s
-      flameLength: dag.node('surface.fire.ellipse.back.flameLength').value(), // ft
-      scorchHeight: dag.node('surface.fire.ellipse.back.scorchHeight').value(), // ft
-      spreadDistance: dag.node('surface.fire.ellipse.back.spreadDistance').value(), // ft
-      spreadRate: dag.node('surface.fire.ellipse.back.spreadRate').value() // ft/min
-    },
-    flanking: {
-      firelineIntensity: dag.node('surface.fire.ellipse.flank.firelineIntensity').value(),
-      flameLength: dag.node('surface.fire.ellipse.flank.flameLength').value(),
-      scorchHeight: dag.node('surface.fire.ellipse.flank.scorchHeight').value(),
-      spreadDistance: dag.node('surface.fire.ellipse.flank.spreadDistance').value(),
-      spreadRate: dag.node('surface.fire.ellipse.flank.spreadRate').value()
-    },
-    heading: {
-      firelineIntensity: dag.node('surface.fire.ellipse.head.firelineIntensity').value(),
-      flameLength: dag.node('surface.fire.ellipse.head.flameLength').value(),
-      scorchHeight: dag.node('surface.fire.ellipse.head.scorchHeight').value(),
-      spreadDistance: dag.node('surface.fire.ellipse.head.spreadDistance').value(),
-      spreadRate: dag.node('surface.fire.ellipse.head.spreadRate').value()
-    }
+    this.dag.select([
+      'surface.primary.fuel.model.behave.parms.cured.herb.fraction', // ratio
+      'surface.primary.fuel.fire.effectiveWindSpeed', // ft/min
+      'surface.primary.fuel.fire.flameResidenceTime', // min
+      'surface.primary.fuel.fire.heading.fromUpslope', // degrees
+      'surface.primary.fuel.fire.heading.fromNorth', // degrees
+      'surface.primary.fuel.fire.heatPerUnitArea', // btu/ft2 |
+      'surface.primary.fuel.fire.reactionIntensity', // btu/ft2/min
+      'surface.fire.ellipse.axis.lengthToWidthRatio', // ratio
+      'surface.fire.ellipse.back.firelineIntensity', // Btu/ft/s
+      'surface.fire.ellipse.back.flameLength', // ft
+      'surface.fire.ellipse.back.scorchHeight', // ft
+      'surface.fire.ellipse.back.spreadDistance', // ft
+      'surface.fire.ellipse.back.spreadRate', // ft/min
+      'surface.fire.ellipse.flank.firelineIntensity',
+      'surface.fire.ellipse.flank.flameLength',
+      'surface.fire.ellipse.flank.scorchHeight',
+      'surface.fire.ellipse.flank.spreadDistance',
+      'surface.fire.ellipse.flank.spreadRate',
+      'surface.fire.ellipse.head.firelineIntensity',
+      'surface.fire.ellipse.head.flameLength',
+      'surface.fire.ellipse.head.scorchHeight',
+      'surface.fire.ellipse.head.spreadDistance',
+      'surface.fire.ellipse.head.spreadRate',
+      'surface.fire.ellipse.size.area', // ft2
+      'surface.fire.ellipse.size.length', // ft
+      'surface.fire.ellipse.size.perimeter', // ft
+      'surface.fire.ellipse.size.width', // ft
+      'site.moisture.dead.tl1h', // ratio
+      'site.moisture.dead.tl10h',
+      'site.moisture.dead.tl100h'
+    ])
   }
-  return output
+
+  showConfigs () {
+    const activeConfigs = this.dag.requiredConfigNodes() // returns an array of DagNode references
+    console.log('ACTIVE CONFIGS:')
+    activeConfigs.forEach(cfg => { console.log(cfg.key(), cfg.value()) })
+  }
+
+  showInputs () {
+    const requiredInputs = this.dag.requiredInputNodes() // returns an array of DagNode references
+    console.log('REQUIRED INPUTS:')
+    requiredInputs.forEach(node => { console.log(node.key()) })
+  }
+
+  run (inp) {
+    this.dag.input([
+      ['surface.primary.fuel.model.catalogKey', [inp.fuel]],
+      ['surface.primary.fuel.model.behave.parms.cured.herb.fraction', [inp.curedHerb]], // fraction
+      ['site.date.month', [inp.month]],
+      ['site.time.hour', [inp.hour]],
+      ['site.location.elevation.diff', [inp.elevDiff]],
+      ['site.slope.direction.aspect', [inp.aspect]], // degrees clockwise from north
+      ['site.slope.steepness.ratio', [inp.slope]], // vertical rise / horizontal reach
+      ['site.temperature.air', [inp.dryBulb]], // oF
+      ['site.temperature.relativeHumidity', [inp.humidity]], // oF
+      ['site.temperature.shading', [inp.shading]], // oF
+      ['site.moisture.live.herb', [inp.liveMoisture]], // fraction of fuel ovendry weight
+      ['site.moisture.live.stem', [inp.liveMoisture]], // fraction of fuel ovendry weight
+      ['site.wind.speed.at10m', [inp.windAt10m]], // feet per minute (1 mph = 88 ft/min)
+      ['site.windSpeedAdjustmentFactor', [inp.windAdj]], // fraction of 10m wind speed
+      ['site.wind.direction.source.fromNorth', [inp.windFrom]], // direction of wind origin, degrees clockwise from north
+      ['site.fire.time.sinceIgnition', [inp.elapsed]] // minutes
+    ]).run()
+
+    const output = {
+      fire: {
+        effectiveWindSpeed: this.dag.node('surface.primary.fuel.fire.effectiveWindSpeed').value(), // ft/min
+        flameResidenceTime: this.dag.node('surface.primary.fuel.fire.flameResidenceTime').value(), // min
+        headingFromUpslope: this.dag.node('surface.primary.fuel.fire.heading.fromUpslope').value(), // degrees
+        headingFromNorth: this.dag.node('surface.primary.fuel.fire.heading.fromNorth').value(), // degrees
+        heatPerUnitArea: this.dag.node('surface.primary.fuel.fire.heatPerUnitArea').value(), // btu/ft2 |
+        reactionIntensity: this.dag.node('surface.primary.fuel.fire.reactionIntensity').value() // btu/ft2/min
+      },
+      moisture: {
+        tl1h: this.dag.node('site.moisture.dead.tl1h').value(),
+        tl10h: this.dag.node('site.moisture.dead.tl10h').value(),
+        tl100h: this.dag.node('site.moisture.dead.tl100h').value(),
+        fosberg: {
+          reference: this.dag.node('site.moisture.dead.fosberg.reference').value(),
+          correction: this.dag.node('site.moisture.dead.fosberg.correction').value(),
+          tl1h: this.dag.node('site.moisture.dead.fosberg.tl1h').value(),
+          tl10h: this.dag.node('site.moisture.dead.fosberg.tl10h').value(),
+          tl100h: this.dag.node('site.moisture.dead.fosberg.tl100h').value()
+        }
+      },
+      ellipse: {
+        lwRatio: this.dag.node('surface.fire.ellipse.axis.lengthToWidthRatio').value(), // ratio
+        area: this.dag.node('surface.fire.ellipse.size.area').value(), // ft2
+        length: this.dag.node('surface.fire.ellipse.size.length').value(), // ft
+        perimeter: this.dag.node('surface.fire.ellipse.size.perimeter').value(), // ft
+        width: this.dag.node('surface.fire.ellipse.size.width').value() // ft
+      },
+      backing: {
+        firelineIntensity: this.dag.node('surface.fire.ellipse.back.firelineIntensity').value(), // Btu/ft/s
+        flameLength: this.dag.node('surface.fire.ellipse.back.flameLength').value(), // ft
+        scorchHeight: this.dag.node('surface.fire.ellipse.back.scorchHeight').value(), // ft
+        spreadDistance: this.dag.node('surface.fire.ellipse.back.spreadDistance').value(), // ft
+        spreadRate: this.dag.node('surface.fire.ellipse.back.spreadRate').value() // ft/min
+      },
+      flanking: {
+        firelineIntensity: this.dag.node('surface.fire.ellipse.flank.firelineIntensity').value(),
+        flameLength: this.dag.node('surface.fire.ellipse.flank.flameLength').value(),
+        scorchHeight: this.dag.node('surface.fire.ellipse.flank.scorchHeight').value(),
+        spreadDistance: this.dag.node('surface.fire.ellipse.flank.spreadDistance').value(),
+        spreadRate: this.dag.node('surface.fire.ellipse.flank.spreadRate').value()
+      },
+      heading: {
+        firelineIntensity: this.dag.node('surface.fire.ellipse.head.firelineIntensity').value(),
+        flameLength: this.dag.node('surface.fire.ellipse.head.flameLength').value(),
+        scorchHeight: this.dag.node('surface.fire.ellipse.head.scorchHeight').value(),
+        spreadDistance: this.dag.node('surface.fire.ellipse.head.spreadDistance').value(),
+        spreadRate: this.dag.node('surface.fire.ellipse.head.spreadRate').value()
+      }
+    }
+    console.log(output.moisture)
+    return output
+  }
 }
+
+// new FireBehavior().showInputs()
