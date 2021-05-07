2d3d9e12151c7ca5bedd416774bc6bd986befb9b
Collin D. Bevins <cbevins@montana.com>
2021-05-07 12:41:01 -0600

    Refactored with parallel requests

--- FireForecast/FireBehaviorForecaster.js
+++ FireForecast/FireBehaviorForecaster.js
@@ -1,13 +1,21 @@
 /**
- * @file fireBehavior.js provides the fire behavior for FireForecast
+ * @file FireBehaviorForecaster.js provides site specific terrain, weather, and fire behavior
+ * for a 48-hour period.
  * @copyright 2021 Systems for Environmental Management
  * @author Collin D. Bevins, <cbevins@montana.com>
  * @license MIT
 */
 import { Sim } from '@cbevins/fire-behavior-simulator'
+import moment from 'moment'
+import { getForecast as getWeatherapi } from './wxQuery-weatherapi.js'
+import { getTimelines as getTomorrow } from './wxQuery-tomorrow.js'
+import { mapquestElevSlopeAspect as mapQuest } from '../Globe/elevQuery-mapquest.js'
+import { usgsElevSlopeAspect as usgs } from '../Globe/elevQuery-usgs.js'
 
-export class FireBehavior {
+export class FireBehaviorForecaster {
   constructor () {
+    this.elevationApi = 'mapquest.com' // or 'usgs.gov'
+    this.weatherApi = 'tomorrow.io' // or 'weatherapi.com'
     this.sim = new Sim()
     this.dag = this.sim.createDag('FireForecast')
     this.dag.configure([
@@ -62,18 +70,55 @@ export class FireBehavior {
     ])
   }
 
-  showConfigs () {
-    const activeConfigs = this.dag.requiredConfigNodes() // returns an array of DagNode references
-    console.log('ACTIVE CONFIGS:')
-    activeConfigs.forEach(cfg => { console.log(cfg.key(), cfg.value()) })
+  /**
+   * Adds fire behavior to the weather records
+   * @param {array} parms Array of input parameters
+   * @param {array} wxArray Array of hourly weather forecast objects
+   * @returns {array} wxArray with 16 or so fire behavior properties added
+   */
+  addFireBehavior (parms, wxArray) {
+    wxArray.forEach(wx => {
+      const input = {
+        fuel: parms.fuel,
+        curedHerb: 0.01 * parms.cured,
+        month: +(wx.date).substr(5, 2),
+        hour: +(wx.time).substr(0, 2),
+        elevDiff: parms.elevdiff,
+        aspect: parms.aspect,
+        slope: 0.01 * parms.slope,
+        dryBulb: wx.dryBulb,
+        humidity: 0.01 * wx.humidity,
+        shading: 0.01 * wx.cloudCover,
+        liveMoisture: 0.01 * parms.live,
+        windAt10m: 88 * wx.windSpeed,
+        windGust: 88 * wx.windGust,
+        windAdj: parms.waf,
+        windFrom: wx.windFrom,
+        elapsed: 60
+      }
+      const output = this.run(input)
+      wx.tl1h = 100 * output.moisture.fosberg.tl1h // ratio
+      wx.tl10h = 100 * output.moisture.tl10h // ratio
+      wx.tl100h = 100 * output.moisture.tl100h // ratio
+      wx.spreadRate = output.heading.spreadRate // ft/min
+      wx.flameLength = output.heading.flameLength // ft
+      wx.scorchHeight = output.heading.scorchHeight // ft
+      wx.headingFromNorth = output.fire.headingFromNorth // degrees
+      wx.gust = {
+        spreadRate: output.heading.gust.spreadRate, // ft/min
+        flameLength: output.heading.gust.flameLength, // ft
+        scorchHeight: output.heading.gust.scorchHeight, // ft
+        headingFromNorth: output.fire.gust.headingFromNorth // degrees
+      }
+    })
+    return wxArray
   }
 
-  showInputs () {
-    const requiredInputs = this.dag.requiredInputNodes() // returns an array of DagNode references
-    console.log('REQUIRED INPUTS:')
-    requiredInputs.forEach(node => { console.log(node.key()) })
-  }
-
+  /**
+   * Gets fire behavior for the supplied inputs
+   * @param {array} inp  Array of fire behavior input values
+   * @returns {object} Fire behavior object
+   */
   run (inp) {
     this.dag.input([
       ['surface.primary.fuel.model.catalogKey', [inp.fuel]],
@@ -145,7 +190,7 @@ export class FireBehavior {
       }
     }
 
-    // ADd fire behavior during wind gusts
+    // Add fire behavior during wind gusts
     this.dag.input([
       ['site.wind.speed.at10m', [inp.windGust]] // feet per minute (1 mph = 88 ft/min)
     ]).run()
@@ -162,6 +207,76 @@ export class FireBehavior {
     }
     return output
   }
-}
 
-// new FireBehavior().showInputs()
+  /**
+   * Display the required configuration nodes
+   */
+  showConfigs () {
+    const activeConfigs = this.dag.requiredConfigNodes() // returns an array of DagNode references
+    console.log('ACTIVE CONFIGS:')
+    activeConfigs.forEach(cfg => { console.log(cfg.key(), cfg.value()) })
+  }
+
+  /**
+   * Display the required input nodes
+   */
+  showInputs () {
+    const requiredInputs = this.dag.requiredInputNodes() // returns an array of DagNode references
+    console.log('REQUIRED INPUTS:')
+    requiredInputs.forEach(node => { console.log(node.key()) })
+  }
+
+  /**
+   * Gets the weather and fire forecast - MAIN ENTRY POINT
+   *
+   * @param {array} parms
+   * - name {string} Location name
+   * - lat {number} Location latitude north (+) or south (-)
+   * - lon {number} Location longitude east (+) or west (-)
+   * - timezone {string} Timezone of time values, according to IANA Timezone Names (defaults to 'UTC')
+   *    (see https://docs.tomorrow.io/reference/api-formats#timezone)
+   * - fuel {string} fuel model key
+   * - waf {number} wind speed adjustment factor from 20-ft to midflame height (fraction)
+   * - cured {number} herb cured fraction (%)
+   * - live {number} live (herb and stem) fuel moisture (%)
+   * - elevdiff {number} ELevation difference between forecast location and site (ft)
+   *
+   * @returns {array} Array of hourly forecast objects
+   */
+  async getForecast (parms) {
+    // configure the time frame up to 6 hours back and 15 days out
+    const now = moment.utc()
+    parms.start = moment.utc(now).startOf('hour').toISOString() // "2019-03-20T14:09:50Z"
+    parms.end = moment.utc(now).add(48, 'hours').toISOString()
+
+    // First get elevation, slope, and aspect and add it to the parms
+    const sampleRes = 1 / (60 * 60 * 3) // 1/3 arc-second in decimal degrees
+    const cellWidth = 2 // Double sample distance to ensure adjacent cells have different sample
+    let _esa
+    if (this.elevationApi === 'usgs.gov') {
+      _esa = usgs(parms.lat, parms.lon, sampleRes, cellWidth)
+    } else { // mapquest.com
+      _esa = mapQuest(parms.lat, parms.lon, sampleRes, cellWidth)
+    }
+
+    // Next get weather data from tomorrow.io or weatherapi.com
+    let _wx
+    if (this.weatherApi === 'weatherapi.com') {
+      _wx = getWeatherapi(parms.lat, parms.lon, 1, 'fire')
+    } else { // tomorrow.io
+      _wx = getTomorrow(parms.lat, parms.lon, parms.start, parms.end, parms.timezone)
+    }
+
+    // Run requests in parallel...
+    const esa = await _esa
+    const wx = await _wx
+
+    parms.elev = esa.elev
+    parms.slope = 100 * esa.slopeRatio
+    parms.aspect = esa.aspect
+
+    // Add fire behavior to the weather record and return
+    this.addFireBehavior(parms, wx)
+    return { parms, wx }
+  }
+}
--- FireForecast/FireForecast.js
+++ FireForecast/FireForecast.js
@@ -1,64 +1,22 @@
 /**
- * FireForecast.js
+ * fireForecast.js
  *
- * Useage: node FireForecast.js
+ * Usage: node fireForecast.js
  *
  * Produces an hourly weather and fire forecast table
  * using world-wide weather forecast data from two alternate sources:
  * - tomorrow.oi
  * - weatherapi.com
  */
-import moment from 'moment'
-import { mapquestElevSlopeAspect as mapQuest } from '../Globe/elevQuery-mapquest.js'
-import { FireBehavior } from './fireBehavior.js'
-import { getTimelines as getTomorrow } from './wxQuery-tomorrow.js'
-// import { getForecast as getWeatherapi } from './wxQuery-weatherapi.js'
-
-// Adds fire behavior to the weather records
-function addFireBehavior (parms, wxArray) {
-  wxArray.forEach(wx => {
-    const input = {
-      fuel: parms.fuel,
-      curedHerb: 0.01 * parms.cured,
-      month: +(wx.date).substr(5, 2),
-      hour: +(wx.time).substr(0, 2),
-      elevDiff: parms.elevdiff,
-      aspect: parms.aspect,
-      slope: 0.01 * parms.slope,
-      dryBulb: wx.dryBulb,
-      humidity: 0.01 * wx.humidity,
-      shading: 0.01 * wx.cloudCover,
-      liveMoisture: 0.01 * parms.live,
-      windAt10m: 88 * wx.windSpeed,
-      windGust: 88 * wx.windGust,
-      windAdj: parms.waf,
-      windFrom: wx.windFrom,
-      elapsed: 60
-    }
-    const output = parms.fireBehavior.run(input)
-    wx.tl1h = 100 * output.moisture.fosberg.tl1h // ratio
-    wx.tl10h = 100 * output.moisture.tl10h // ratio
-    wx.tl100h = 100 * output.moisture.tl100h // ratio
-    wx.spreadRate = output.heading.spreadRate // ft/min
-    wx.flameLength = output.heading.flameLength // ft
-    wx.scorchHeight = output.heading.scorchHeight // ft
-    wx.headingFromNorth = output.fire.headingFromNorth // degrees
-    wx.gust = {
-      spreadRate: output.heading.gust.spreadRate, // ft/min
-      flameLength: output.heading.gust.flameLength, // ft
-      scorchHeight: output.heading.gust.scorchHeight, // ft
-      headingFromNorth: output.fire.gust.headingFromNorth // degrees
-    }
-  })
-  return wxArray
-}
+import { FireBehaviorForecaster } from './FireBehaviorForecaster.js'
 
 function fix (v, w, d, c = ' ') {
   return (typeof v === 'string') ? v.padStart(w, c) : v.toFixed(d).padStart(w, c)
 }
 
 // Returns the forecast table string
-function getForecastTable (parms, wxArray) {
+function getForecastTable (forecast) {
+  const parms = forecast.parms
   const h0 = '|-------|---------|-----------|-----|-------------|---------------------------|---------------------------|\n'
   const h1 = '|  Time |  Db  Rh | Wind mph  | Cld |  Dead Fuel  | Spread  Flame Scorch Head | Spread  Flame Scorch Head |\n'
   const h2 = '|       |  oF   % | Sp Gs Dir | Cvr | 1h 10h 100h | ft/min     ft     ft  No  |      During Wind Gusts    |\n'
@@ -69,7 +27,7 @@ function getForecastTable (parms, wxArray) {
   str += `${fix(parms.elev, 4, 0)}${fix(parms.slope, 5, 0)}${fix(parms.aspect, 5, 0)}`
   str += `${fix(parms.fuel, 5)}${fix(parms.cured, 5, 2)}${fix(parms.live, 5, 0)}${fix(parms.waf, 6, 2)}\n\n`
   let lastDate = ''
-  wxArray.forEach(w => {
+  forecast.wx.forEach(w => {
     if (w.date !== lastDate) {
       if (lastDate !== '') str += h0
       str += `\n  ${w.date}\n` + h0 + h1 + h2 + h0
@@ -99,46 +57,20 @@ function getForecastTable (parms, wxArray) {
   return str
 }
 
-async function fireForecast (parms) {
-  // First get elevation, slope, and aspect and add it to the parms
-  const sampleRes = 1 / (60 * 60 * 3) // 1/3 arc-second in decimal degrees
-  const cellWidth = 2 // Double sample distance to ensure adjacent cells have different sample
-  const esa = await mapQuest(parms.lat, parms.lon, sampleRes, cellWidth)
-  parms.elev = esa.elev
-  parms.slope = 100 * esa.slopeRatio
-  parms.aspect = esa.aspect
-
-  // Next get weather data from tomorrow.io or weatherapi.com
-  const wxArray = await getTomorrow(parms.lat, parms.lon, parms.start, parms.end, parms.timezone)
-  // const wxArray = await getWeatherapi(parms.lat, parms.lon, 1, 'fire')
-  addFireBehavior(parms, wxArray)
-  const table = getForecastTable(parms, wxArray)
-  console.log(table)
-}
-
 // ----------------------------------------------------------------
 // Main CLI
 // ----------------------------------------------------------------
+
 const M = { name: 'The "M"', lat: 46.859340, lon: -113.975528 }
 // const Home = { name: 'Home', lat: 46.85714, lon: -114.00730 }
 const loc = M
-// Define the command line parameters and their defaults
-// configure the time frame up to 6 hours back and 15 days out
-const now = moment.utc()
 const parms = {
   name: loc.name,
-  // weather and elevation service input parameters
   lat: loc.lat,
   lon: loc.lon,
-  start: moment.utc(now).startOf('hour').toISOString(), // "2019-03-20T14:09:50Z"
-  end: moment.utc(now).add(48, 'hours').toISOString(),
-  // Timezone of time values, according to IANA Timezone Names (defaults to 'UTC')
-  // https://docs.tomorrow.io/reference/api-formats#timezone
   timezone: 'America/Denver',
-
-  // fire behavior input parameters
-  fuel: 'gs1',
-  waf: 0.4,
+  fuel: 'gs1', // fuel model key
+  waf: 0.4, // wind speed adjustment factor from 20-ft to midflame height
   cured: 0, // herb cured fraction (%)
   live: 200, // live herb and stem fuel moisture (%)
   elevdiff: 0
@@ -154,7 +86,15 @@ for (let a = 2; a < process.argv.length; a++) {
     parms.set(part[0], part[1])
   }
 }
+
 // \TODO - validate command line args
-parms.fireBehavior = new FireBehavior()
-fireForecast(parms)
+async function showForecast (forecaster, parms) {
+  const forecast = await forecaster.getForecast(parms)
+  const table = getForecastTable(forecast)
+  console.log(table)
+  return forecast
+}
+
+const forecaster = new FireBehaviorForecaster()
+showForecast(forecaster, parms)
 console.log('Getting elevation and weather data ...')
