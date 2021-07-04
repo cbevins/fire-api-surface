4184167cd43e966bdf9c13660f7bb0b129580338
Collin D. Bevins <cbevins@montana.com>
2021-07-04 13:30:06 -0600

    Reordered timeRes and scanSpacing args

--- FireEllipse/FireLandscape.js
+++ FireEllipse/FireLandscape.js
@@ -3,7 +3,7 @@ import { AbstractLandscape } from './AbstractLandscape.js'
 import { FireBehaviorProvider } from './FireBehaviorProvider.js'
 
 // Note: we could have any positive number for Burned, such as the burn period,
-// but that would lead to highly fractured scanLine with about 2*timeSteps segments each
+// but that would lead to highly fractured scanline with about 2*timeSteps segments each
 export const Unburnable = -2
 export const Unburned = 0
 export const Burned = 1
@@ -11,25 +11,25 @@ export const Burning = 2
 export const Edge = -1
 
 export class FireLandscape extends AbstractLandscape {
-  constructor (sceneWidth, sceneHeight, scanSpacing = 1, timeRes = 1) {
+  constructor (sceneWidth, sceneHeight, timeRes = 1, scanSpacing = 1) {
     super(sceneWidth, sceneHeight)
     this._scanSpacing = scanSpacing // spacing between scan lines in common units
     this._timeRes = timeRes // simulation time step in common units (i.e., min)
     this._provider = new FireBehaviorProvider(this)
-    this._hLines = [] // An array of Scanlines
+    this._hlines = [] // An array of Scanlines
     // Each Scanline is [<yPos>, [<segment>]] and always ends with a [<pos>, Edge] segment
     for (let y = 0; y <= sceneHeight; y += scanSpacing) {
-      this._hLines.push({
+      this._hlines.push({
         at: y,
         segments: [
           { starts: 0, status: Unburned },
           { starts: sceneWidth, status: Edge }]
       })
     }
-    this._vLines = [] // An array of Scanlines
+    this._vlines = [] // An array of Scanlines
     // Each Scanline is [<xPos>, [<segment>]] and always ends with a [<xpos>, Edge] segment
     for (let x = 0; x <= sceneWidth; x += scanSpacing) {
-      this._vLines.push({
+      this._vlines.push({
         at: x,
         segments: [
           { starts: 0, status: Unburned },
@@ -55,59 +55,90 @@ export class FireLandscape extends AbstractLandscape {
     }
 
     const fire = this._provider.fireBehavior(input)
-    fire.length = this._timeStep * (fire.headRos + fire.backRos)
-    fire.width = this._timeStep * 2 * fire.flankRos
+    fire.length = this._timeRes * (fire.headRos + fire.backRos)
+    fire.width = this._timeRes * 2 * fire.flankRos
     return fire
   }
 
-  hLines () { return this._hLines }
+  // Returns index of the vlines at x
+  col (x) {
+    x = Math.max(0, x)
+    x = Math.min(x, this._sceneWidth)
+    return Math.floor(x / this._scanSpacing)
+  }
+
+  // Returns the index of the hline at y
+  row (y) {
+    y = Math.max(0, y)
+    y = Math.min(y, this._sceneHeight)
+    return Math.floor(y / this._scanSpacing)
+  }
+
+  // Returns a reference to the horizontal scan line at idx
+  hline (idx) { return this._hlines[idx] }
+
+  // Returns a reference to the array of horizontal scan lines
+  hlines () { return this._hlines }
+
+  // Returns an array of all the scaline's burned-unbured and unburned-burned interfaces
+  scanlineFireFronts (scanline) {
+    const pts = []
+    scanline.segments.forEach((segment, idx) => {
+      if (segment.status === Burned) {
+        if (idx && scanline.segments[idx - 1].status === Unburned) pts.push(segment.starts)
+        if (scanline.segments[idx + 1].status === Unburned) pts.push(scanline.segments[idx + 1].starts)
+      }
+    })
+    return pts
+  }
 
   // Returns FireGrid point spacing (same for both rows and cols)
   scanSpacing () { return this._scanSpacing }
 
+  // Returns the time resolution
   timeRes () { return this._timeRes }
 
   /**
    * Updates a scanline with fire in those segments that are currently unburned
    * @param {number} fireBegins Start of the fire segment
    * @param {number} fireEnds End of fire segment
-   * @param {number} scanLine Scanline to to be updated
+   * @param {number} scanline Scanline to to be updated
    */
-  updateScanlineWithFire (fireBegins, fireEnds, scanLine) {
+  updateScanlineWithFire (fireBegins, fireEnds, scanline) {
     const updated = [] // starts with an empty, updated segment
-    // Examine each unburned segment of the scanLine
-    scanLine.segments.forEach((segment, idx) => {
+    // Examine each unburned segment of the scanline
+    scanline.segments.forEach((segment, idx) => {
       // Case 1: this scanline segment is either already Burned or Unburnable
-      // add this scanline segment as-is to the updated segment
+      // adds this scanline segment as-is to the updated segment
       if (segment.status !== Unburned) {
         this.updateSegment(updated, segment)
       } else {
         // This scanline segment ends at the start of the next scanline segment
-        const segEnds = scanLine.segments[idx + 1].starts
-        // Case 2: The fire segment and this scanLine segments do not overlap
-        // add this scanline segment as-is to the updated segment
+        const segEnds = scanline.segments[idx + 1].starts
+        // Case 2: The fire segment and this scanline segments do not overlap
+        // adds this scanline segment as-is to the updated segment
         if (fireEnds < segment.starts || fireBegins > segEnds) {
           this.updateSegment(updated, segment)
         }
-        // Case 3: The fire segment completely covers this scanLine segment
-        // update this scanline segment to Burned and add it to the updated segment
+        // Case 3: The fire segment completely covers this scanline segment
+        // updates this scanline segment to Burned and add it to the updated segment
         else if (fireBegins <= segment.starts && fireEnds >= segEnds) {
           this.updateSegment(updated, { starts: segment.starts, status: Burned })
         }
-        // Case 4: The fire segment starts before this scanLine segment and ends within it
-        // add a Burned and an Unburned segment to the update
+        // Case 4: The fire segment starts before this scanline segment and ends within it
+        // adds a Burned and an Unburned segment to the update
         else if (fireBegins <= segment.starts && fireEnds < segEnds) {
           this.updateSegment(updated, { starts: segment.starts, status: Burned })
           this.updateSegment(updated, { starts: fireEnds, status: Unburned })
         }
-        // Case 5: The fire segment starts within this scanLine segment and extends beyond it
-        // add an Unburned and a Burned segment to the update
+        // Case 5: The fire segment starts within this scanline segment and extends beyond it
+        // adds an Unburned and a Burned segment to the update
         else if (fireBegins > segment.starts && fireBegins < segEnds && fireEnds >= segEnds) {
           this.updateSegment(updated, { starts: segment.starts, status: Unburned })
           this.updateSegment(updated, { starts: fireBegins, status: Burned })
         }
-        // Case 6: The fire segment lies entirely within this scanLine segment
-        // add an Unburned, a Burned, and another Unburned segment to the update
+        // Case 6: The fire segment lies entirely within this scanline segment
+        // adds an Unburned, a Burned, and another Unburned segment to the update
         else if (fireBegins > segment.starts && fireBegins < segEnds && fireEnds < segEnds) {
           this.updateSegment(updated, { starts: segment.starts, status: Unburned })
           this.updateSegment(updated, { starts: fireBegins, status: Burned })
@@ -117,8 +148,8 @@ export class FireLandscape extends AbstractLandscape {
         }
       }
     })
-    // return an updated scanLine
-    return { at: scanLine.at, segments: updated }
+    // return an updated scanline
+    return { at: scanline.at, segments: updated }
   }
 
   // Adds the segment to the array only if the segment's status differs from the last segment in the array
@@ -126,5 +157,9 @@ export class FireLandscape extends AbstractLandscape {
     if (!a.length || a[a.length - 1].status !== segment.status) a.push(segment)
   }
 
-  vLines () { return this._vLines }
+  // Returns a reference to the vertical scan line at idx
+  vline (idx) { return this._vlines[idx] }
+
+  // Returns a reference to the array of vertical scan lines
+  vlines () { return this._vlines }
 }
--- FireEllipse/FireLandscape.test.js
+++ FireEllipse/FireLandscape.test.js
@@ -1,86 +1,175 @@
 /* eslint-disable jest/prefer-to-have-length */
 import { FireLandscape, Burned, Unburned, Edge } from './FireLandscape.js'
+import { FireWavelet } from './FireWavelet.js'
+
+function fmt (x, y) { return `[${x.toFixed(2)}, ${y.toFixed(2)}]` }
 
 test('1: FireLandscape constructor', () => {
   const width = 4000
   const height = 5000
   const spacing = 10
   const timeRes = 2
-  const grid = new FireLandscape(width, height, spacing, timeRes)
-  expect(grid instanceof FireLandscape).toEqual(true)
-  expect(grid.sceneWidth()).toEqual(width)
-  expect(grid.sceneHeight()).toEqual(height)
-  expect(grid.scanSpacing()).toEqual(10)
-  expect(grid.timeRes()).toEqual(2)
-  expect(grid.hLines().length).toEqual(501)
-  expect(grid.vLines().length).toEqual(401)
+  const fl = new FireLandscape(width, height, timeRes, spacing)
+  expect(fl instanceof FireLandscape).toEqual(true)
+  expect(fl.sceneWidth()).toEqual(width)
+  expect(fl.sceneHeight()).toEqual(height)
+  expect(fl.scanSpacing()).toEqual(10)
+  expect(fl.timeRes()).toEqual(2)
+  expect(fl.hlines().length).toEqual(501)
+  expect(fl.vlines().length).toEqual(401)
 })
 
-test('2: FireLandscape scanLines', () => {
+test('2: FireLandscape scanlines', () => {
   const width = 4000
   const height = 5000
   const spacing = 1
   const timeRes = 1
-  const grid = new FireLandscape(width, height, spacing, timeRes)
+  const fl = new FireLandscape(width, height, timeRes, spacing)
 
-  // Start with a single Unburned scanLine segment
-  let dup = grid.hLines()[2000]
-  expect(dup.at).toEqual(2000)
-  expect(dup.segments.length).toEqual(2)
-  expect(dup.segments[0]).toEqual({ starts: 0, status: Unburned })
-  expect(dup.segments[1]).toEqual({ starts: 4000, status: Edge })
+  // Start with a single Unburned scanline segment
+  let scanline = fl.hlines()[2000]
+  expect(scanline.at).toEqual(2000)
+  expect(scanline.segments.length).toEqual(2)
+  expect(scanline.segments[0]).toEqual({ starts: 0, status: Unburned })
+  expect(scanline.segments[1]).toEqual({ starts: 4000, status: Edge })
+  expect(fl.scanlineFireFronts(scanline)).toEqual([])
 
-  // The fire segment lies entirely within an Unburned scanLine segment
+  // The fire segment lies entirely within an Unburned scanline segment
   // Should replace the segment with an Unburned, a Burned, and another Unburned
-  dup = grid.updateScanlineWithFire(1000, 2000, dup)
-  expect(dup.segments.length).toEqual(4)
-  expect(dup.segments[0]).toEqual({ starts: 0, status: Unburned })
-  expect(dup.segments[1]).toEqual({ starts: 1000, status: Burned })
-  expect(dup.segments[2]).toEqual({ starts: 2000, status: Unburned })
-  expect(dup.segments[3]).toEqual({ starts: 4000, status: Edge })
+  scanline = fl.updateScanlineWithFire(1000, 2000, scanline)
+  expect(scanline.segments.length).toEqual(4)
+  expect(scanline.segments[0]).toEqual({ starts: 0, status: Unburned })
+  expect(scanline.segments[1]).toEqual({ starts: 1000, status: Burned })
+  expect(scanline.segments[2]).toEqual({ starts: 2000, status: Unburned })
+  expect(scanline.segments[3]).toEqual({ starts: 4000, status: Edge })
+  expect(fl.scanlineFireFronts(scanline)).toEqual([1000, 2000])
 
-  // The fire segment lies entirely within an Burned scanLine segment
+  // The fire segment lies entirely within an Burned scanline segment
   // Should be no changes
-  dup = grid.updateScanlineWithFire(1500, 1600, dup)
-  expect(dup.segments.length).toEqual(4)
-  expect(dup.segments[0]).toEqual({ starts: 0, status: Unburned })
-  expect(dup.segments[1]).toEqual({ starts: 1000, status: Burned })
-  expect(dup.segments[2]).toEqual({ starts: 2000, status: Unburned })
-  expect(dup.segments[3]).toEqual({ starts: 4000, status: Edge })
+  scanline = fl.updateScanlineWithFire(1500, 1600, scanline)
+  expect(scanline.segments.length).toEqual(4)
+  expect(scanline.segments[0]).toEqual({ starts: 0, status: Unburned })
+  expect(scanline.segments[1]).toEqual({ starts: 1000, status: Burned })
+  expect(scanline.segments[2]).toEqual({ starts: 2000, status: Unburned })
+  expect(scanline.segments[3]).toEqual({ starts: 4000, status: Edge })
+  expect(fl.scanlineFireFronts(scanline)).toEqual([1000, 2000])
 
-  // The fire segment starts within an Unburned and ends within a Burned scanLine segment
+  // The fire segment starts within an Unburned and ends within a Burned scanline segment
   // Should start the Burned segment sooner without adding any segments
-  dup = grid.updateScanlineWithFire(500, 1500, dup)
-  expect(dup.segments.length).toEqual(4)
-  expect(dup.segments[0]).toEqual({ starts: 0, status: Unburned })
-  expect(dup.segments[1]).toEqual({ starts: 500, status: Burned })
-  expect(dup.segments[2]).toEqual({ starts: 2000, status: Unburned })
-  expect(dup.segments[3]).toEqual({ starts: 4000, status: Edge })
+  scanline = fl.updateScanlineWithFire(500, 1500, scanline)
+  expect(scanline.segments.length).toEqual(4)
+  expect(scanline.segments[0]).toEqual({ starts: 0, status: Unburned })
+  expect(scanline.segments[1]).toEqual({ starts: 500, status: Burned })
+  expect(scanline.segments[2]).toEqual({ starts: 2000, status: Unburned })
+  expect(scanline.segments[3]).toEqual({ starts: 4000, status: Edge })
+  expect(fl.scanlineFireFronts(scanline)).toEqual([500, 2000])
 
   // The fire segment starts within a Burned and ends within an Unburned scanline segment
   // The Unburned sgement should start later without adding any segments
-  dup = grid.updateScanlineWithFire(1500, 2500, dup)
-  expect(dup.segments.length).toEqual(4)
-  expect(dup.segments[0]).toEqual({ starts: 0, status: Unburned })
-  expect(dup.segments[1]).toEqual({ starts: 500, status: Burned })
-  expect(dup.segments[2]).toEqual({ starts: 2500, status: Unburned })
-  expect(dup.segments[3]).toEqual({ starts: 4000, status: Edge })
+  scanline = fl.updateScanlineWithFire(1500, 2500, scanline)
+  expect(scanline.segments.length).toEqual(4)
+  expect(scanline.segments[0]).toEqual({ starts: 0, status: Unburned })
+  expect(scanline.segments[1]).toEqual({ starts: 500, status: Burned })
+  expect(scanline.segments[2]).toEqual({ starts: 2500, status: Unburned })
+  expect(scanline.segments[3]).toEqual({ starts: 4000, status: Edge })
+  expect(fl.scanlineFireFronts(scanline)).toEqual([500, 2500])
 
   // Start a second fire run
   // Should add 2 more segments
-  dup = grid.updateScanlineWithFire(3000, 3500, dup)
-  expect(dup.segments.length).toEqual(6)
-  expect(dup.segments[0]).toEqual({ starts: 0, status: Unburned })
-  expect(dup.segments[1]).toEqual({ starts: 500, status: Burned })
-  expect(dup.segments[2]).toEqual({ starts: 2500, status: Unburned })
-  expect(dup.segments[3]).toEqual({ starts: 3000, status: Burned })
-  expect(dup.segments[4]).toEqual({ starts: 3500, status: Unburned })
-  expect(dup.segments[5]).toEqual({ starts: 4000, status: Edge })
+  scanline = fl.updateScanlineWithFire(3000, 3500, scanline)
+  expect(scanline.segments.length).toEqual(6)
+  expect(scanline.segments[0]).toEqual({ starts: 0, status: Unburned })
+  expect(scanline.segments[1]).toEqual({ starts: 500, status: Burned })
+  expect(scanline.segments[2]).toEqual({ starts: 2500, status: Unburned })
+  expect(scanline.segments[3]).toEqual({ starts: 3000, status: Burned })
+  expect(scanline.segments[4]).toEqual({ starts: 3500, status: Unburned })
+  expect(scanline.segments[5]).toEqual({ starts: 4000, status: Edge })
+  expect(fl.scanlineFireFronts(scanline)).toEqual([500, 2500, 3000, 3500])
 
   // Fire over entire scanline
   // SHould end up with just Unburned and Edge segments
-  dup = grid.updateScanlineWithFire(-9999, 99999, dup)
-  expect(dup.segments.length).toEqual(2)
-  expect(dup.segments[0]).toEqual({ starts: 0, status: Burned })
-  expect(dup.segments[1]).toEqual({ starts: 4000, status: Edge })
+  scanline = fl.updateScanlineWithFire(-9999, 99999, scanline)
+  expect(scanline.segments.length).toEqual(2)
+  expect(scanline.segments[0]).toEqual({ starts: 0, status: Burned })
+  expect(scanline.segments[1]).toEqual({ starts: 4000, status: Edge })
+  expect(fl.scanlineFireFronts(scanline)).toEqual([])
+})
+
+test('3: FireLandscape.fireBehaviorAt()', () => {
+  const width = 4000
+  const height = 5000
+  const spacing = 1
+  const timeRes = 1
+  const fl = new FireLandscape(width, height, timeRes, spacing)
+
+  const fire = fl.fireBehaviorAt(0, 0)
+  console.log(fire)
+  expect(fire.key).toEqual('124|0.778|0.05|0.07|0.09|0.50|1.50|0.25|135|315|880')
+  expect(fire.lwr).toEqual(3.423598931799779)
+  expect(fire.backRos).toEqual(1.0358737174389132)
+  expect(fire.flankRos).toEqual(6.938175750154048)
+  expect(fire.headRos).toEqual(46.47118845629415)
+  expect(fire.heading).toEqual(135)
+  expect(fire.length).toEqual(47.50706217373306)
+  expect(fire.width).toEqual(13.876351500308097)
+})
+
+test('4: FireLandscape col() and row()', () => {
+  const width = 4000
+  const height = 5000
+  const spacing = 5
+  const timeRes = 1
+  const fl = new FireLandscape(width, height, timeRes, spacing)
+  expect(fl.hlines().length).toEqual(1 + height / 5)
+  expect(fl.col(0)).toEqual(0)
+  expect(fl.col(-999999)).toEqual(0)
+  expect(fl.col(2000)).toEqual(2000 / 5)
+  expect(fl.col(4000)).toEqual(4000 / 5)
+  expect(fl.col(999999)).toEqual(4000 / 5)
+})
+
+test('5: FireLandscape FireWavelet', () => {
+  const width = 4000
+  const height = 5000
+  const spacing = 5
+  const timeRes = 1
+  const fl = new FireLandscape(width, height, timeRes, spacing)
+
+  const fwLength = 100
+  const fwWidth = 50
+  const fwDegrees = 135
+  const verbose = false
+  const fw = new FireWavelet(fwLength, fwWidth, fwDegrees, timeRes, spacing, verbose)
+  expect(fw.a()).toEqual(50)
+  expect(fw.b()).toEqual(25)
+  expect(fw.headRate()).toBeCloseTo(93.3012701892219, 12)
+  expect(fw.hOrigin()).toEqual(1)
+  expect(fw.vOrigin()).toEqual(1)
+
+  // Set an ignition point
+  const ix = 2000
+  const iy = 2500
+  // let str = 'yFw yFl yIdx\n'
+  fw.hlines().forEach(([ypos, x1, x2]) => {
+    const idx = fl.row(iy + ypos)
+    expect(idx).toEqual(Math.ceil(ypos + iy) / spacing)
+    let scanline = fl.hline(idx)
+    expect(scanline.segments.length).toEqual(2)
+    scanline = fl.updateScanlineWithFire(x1 + ix, x2 + ix, scanline)
+    console.log(scanline.segments)
+    expect(scanline.segments.length).toEqual(4) // Unburned, Burned, Unburned, Edge
+    // str += `${Math.trunc(ypos)} ${Math.trunc(ypos + iy)} ${idx}\n`
+  })
+  // console.log(str)
+
+  // let str = 'xFw xFl xIdx\n'
+  // fw.vlines().forEach(([xpos, y1, y2]) => {
+  //   const idx = fl.col(ix + xpos)
+  //   expect(idx).toEqual(Math.ceil(xpos + ix) / spacing)
+  //   let scanline = fl.vline(idx)
+  //   expect(scanline.segments.length).toEqual(2)
+  //   scanline = fl.updateScanlineWithFire(y1+iy, y2+iy, scanline)
+  //   str += `${Math.trunc(xpos)} ${Math.trunc(xpos + ix)} ${idx}\n`
+  // })
+  // console.log(str)
 })
